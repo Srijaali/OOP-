3d8123232c856d876f39aef4eed1020c1a54ef3d
Syeda Rija Ali <142867637+Srijaali@users.noreply.github.com>
2024-02-09 20:02:14 +0500

    Update q1.cpp

--- lab2/q1.cpp
+++ lab2/q1.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+using namespace std;
 
+int recursiveswap(int& a,int& b){
+   int temp;
+    if(a==b){
+        return 0;//base case to return/exit if the integers are same
+    }
+    else {
+        
+        temp = a;
+        a = b;
+        b = temp;
+    recursiveswap(b,a)
+
+    }
+ 
+}
+
+int main() {
+    int a = 6;
+    int b = 2;
+    int temp;
+
+    cout << "Before swapping: \n";
+    cout << "A = " << a << "\n";
+      cout << "B = " << b << endl;
+
+      recursiveswap(a,b);
+
+      cout<< "After swapping:\n";
+      cout<< "A = " << a << "\n";
+        cout<< "B = " << b << "\n";
+
+}
