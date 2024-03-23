041c93bd6e0b797c09e3c6004e64d3da93b6f7b3
Syeda Rija Ali <142867637+Srijaali@users.noreply.github.com>
2024-03-23 23:14:05 +0500

    Update q1.cpp

--- lab8/q1.cpp
+++ lab8/q1.cpp
@@ -1,1 +1,54 @@
+/*
+Name: Syeda Rija Ali
+ID:23K-0057
+desc: simple inheritance between book and fiction book class
+*/
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//base class 
+class Book {
+	protected:
+    	string title;
+    	string author;
+    	string publisher;
+
+	public:
+    Book(string title, string author, string publisher)
+        : title(title), author(author), publisher(publisher) {}
+
+    void display() const {
+    	cout<<"Displaying book information:"<<endl;
+        cout<<"Title: "<<title<<endl;
+        cout<<"Author: "<<author << endl;
+        cout<<"Publisher: "<<publisher<<endl;
+    }
+};
+
+//derived
+class FictionBook : public Book {
+	private:
+    	string genre;
+    	string protagonist;
+
+	public:
+    	FictionBook(string title, string author, string publisher, string genre, string protagonist)
+        : Book(title, author, publisher), genre(genre), protagonist(protagonist) {}
+
+    void display() const {
+        Book::display();
+        cout<<"Genre: "<<genre<<endl;
+        cout<<"Protagonist: "<<protagonist<<endl;
+    }
+};
+
+int main() {
+    //instance of the derived class
+    FictionBook book("Harry Potter and the Philospher's Stone", "J.K.Rowling", "oxford", "Fiction", "Harry Potter");
+    book.display();
+
+    return 0;
+}
