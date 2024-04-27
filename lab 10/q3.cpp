e05b5c857f1b7a3190f5f9338522d420d04871f2
Syeda Rija Ali <142867637+Srijaali@users.noreply.github.com>
2024-04-27 19:14:47 +0500

    Create q3.cpp

---
+++ lab 10/q3.cpp
@@ -0,0 +1,48 @@
+/*
+Name: Syeda Rija Ali
+ID: 23K-0057
+Desc:  operator overloading.
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Shape{
+    int length;
+    int breadth;
+    int area;
+
+    public:
+    Shape(int length, int breadth) : length(length) , breadth(breadth) {
+        area =  length*breadth;
+    }
+
+    Shape operator+(const Shape& s) const {
+       Shape temp(length + s.length , breadth +s.breadth );
+        temp.area = area + s.area;
+        return temp;
+       
+    }
+
+    void Display()const{
+        cout << "Area is: " << area << endl;
+    }
+}; 
+
+int main(){
+    Shape shape1(10,12) , shape2(4,8);
+    
+    shape1.Display();
+    shape2.Display();
+
+    Shape shape3 = shape1 + shape2;
+   // shape3 = shape1 + shape2;
+   cout << "Combined" ;
+   shape3.Display();
+
+   return 0;
+    
+    //shape3.area();
+
+}
